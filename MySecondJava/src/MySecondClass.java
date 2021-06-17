9447c3301ecc869f717eef656ec63e27dc4c1534
TJ <TJ@DESKTOP-UOO4N74>
2021-06-17 21:48:14 +0900

    fix / class day02 / MySecondClass Line13 update fix / class day02 / MySecondClass Line14-20 add

--- MySecondJava/src/MySecondClass.java
+++ MySecondJava/src/MySecondClass.java
@@ -12,5 +12,11 @@ public class MySecondClass {
 	public static void main(String[] args) {
 		System.out.println("Hello World2");
 		System.out.println("Hello World2");
+		System.out.println("Hello World2");
+		System.out.println("Hello World2");
+		System.out.println("Hello World2");
+		System.out.println("Hello World2");
+		System.out.println("Hello World2");
+		System.out.println("Hello World2");
 	}
 }
