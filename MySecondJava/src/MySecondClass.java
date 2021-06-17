598275c069db7e2e3d7ae853e19f109fddc8162d
TJ <TJ@DESKTOP-UOO4N74>
2021-06-17 21:50:24 +0900

    fix / class day02 / MySecondClass Line 13-20 수정

--- MySecondJava/src/MySecondClass.java
+++ MySecondJava/src/MySecondClass.java
@@ -10,7 +10,7 @@ public class MySecondClass {
 	 * @param print string
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello World23456");
+		System.out.println("Hello World2345678");
 		System.out.println("Hello World2");
 		System.out.println("Hello World2");
 		System.out.println("Hello World2");
